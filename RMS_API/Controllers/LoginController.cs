69c761c3a7b0f5fc01a6c5e1c9fc60c2d82722bd
0ankit0 <ankitpaudel.1373@gmail.com>
2024-07-31 07:49:25 +0545

    Add LoginController, popover menu, and update libraries
    - Introduced `LoginController` with `UserLogin` endpoint using `IJwtAuth`.
    - Updated `Index.cshtml` with a button and popover menu, and included `customlibuse.css` and `customlibraryuse.js`.
    - Modified `_Layout.cshtml` to include `popper.js` and adjusted script order.
    - Updated `_ValidationScriptsPartial.cshtml` with new paths for validation scripts.
    - Revised `libman.json` to update and add libraries (`bootstrap`, `jquery-validate`, `jquery-validation-unobtrusive`, `popper.js`).
    - Added `CustomLibUse.css` for button and popover menu styles.
    - Added `CustomLibraryUse.js` for popover functionality using `popper.js`.

---
+++ RMS_API/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RMS_API.CustomClass;
+using RMS_API.Models;
+using System.Data;
+using System.Text;
+
+namespace RMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IJwtAuth _jwtAuth;
+
+        public LoginController(IJwtAuth jwtAuth)
+        {
+            _jwtAuth = jwtAuth;
+        }
+
+        // POST: api/Login/UserLogin
+        [HttpPost("UserLogin")]
+        public IActionResult UserLogin([FromBody] LoginModel br)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+        }
+    }
+}
---
+++ RMS_FRONTEND/wwwroot/js/CustomLibraryUse.js
@@ -0,0 +1,64 @@
+﻿const popperButton = document.querySelector("#popper-button");
+const popperPopup = document.querySelector("#popper-popup");
+const popperSection = document.querySelector("#popper-section");
+const popperArrow = document.querySelector("#popper-arrow");
+
+let popperInstance = null;
+
+//create popper instance
+function createInstance() {
+    popperInstance = Popper.createPopper(popperButton, popperPopup, {
+        placement: "bottom", //preferred placement of popper
+        modifiers: [
+            {
+                name: "offset", //offsets popper from the reference/button
+                options: {
+                    offset: [0, 8]
+                }
+            },
+            {
+                name: "flip", //flips popper with allowed placements
+                options: {
+                    allowedAutoPlacements: ["right", "left", "top", "bottom"],
+                    rootBoundary: "viewport"
+                }
+            }
+        ]
+    });
+}
+
+//destroy popper instance
+function destroyInstance() {
+    if (popperInstance) {
+        popperInstance.destroy();
+        popperInstance = null;
+    }
+}
+
+//show and create popper
+function showPopper() {
+    popperPopup.setAttribute("show-popper", "");
+    popperArrow.setAttribute("data-popper-arrow", "");
+    createInstance();
+}
+
+//hide and destroy popper instance
+function hidePopper() {
+    popperPopup.removeAttribute("show-popper");
+    popperArrow.removeAttribute("data-popper-arrow");
+    destroyInstance();
+}
+
+//toggle show-popper attribute on popper to hide or show it with CSS
+function togglePopper() {
+    if (popperPopup.hasAttribute("show-popper")) {
+        hidePopper();
+    } else {
+        showPopper();
+    }
+}
+//execute togglePopper function when clicking the popper reference/button
+popperButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    togglePopper();
+});
