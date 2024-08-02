e691ed98c979ee56b929784608abe3c844be8b0f
0ankit0 <ankitpaudel.1373@gmail.com>
2024-08-02 16:50:07 +0545

    Add custom memory cache implementation
    Introduce a new custom memory cache in the `RMS_API.CustomClass` namespace.
    Define `ICustomMemoryCache` interface for cache operations.
    Implement `MemoryCache` class using `IMemoryCache` from `Microsoft.Extensions.Caching.Memory`.
    Support optional sliding and absolute expiration times in `Set` method.

---
+++ RMS_API/CustomClass/MemoryCache.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace RMS_API.CustomClass
+{
+    public interface ICustomMemoryCache
+    {
+        T Get<T>(string key);
+        void Set<T>(string key, T value, TimeSpan? slidingExpiration = null, DateTimeOffset? absoluteExpiration = null);
+        void Remove(string key);
+    }
+    public class MemoryCache : ICustomMemoryCache
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public MemoryCache(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public void Set<T>(string key, T value, TimeSpan? slidingExpiration = null, DateTimeOffset? absoluteExpiration = null)
+        {
+            var options = new MemoryCacheEntryOptions();
+
+            if (slidingExpiration.HasValue)
+            {
+                options.SetSlidingExpiration(slidingExpiration.Value);
+            }
+
+            if (absoluteExpiration.HasValue)
+            {
+                options.SetAbsoluteExpiration(absoluteExpiration.Value);
+            }
+
+            _memoryCache.Set(key, value, options);
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+    }
+}
