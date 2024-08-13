2dc95f2d415e5764a84b18044a3616953ac8f829
0ankit0 <ankitpaudel.1373@gmail.com>
2024-08-13 08:23:26 +0545

    Add new packages, DataTable, and custom scripts/styles
    Updated RMS_API.csproj to include new package references:
    DotNetRateLimiter, JWT, Microsoft.AspNetCore.Authentication.JwtBearer,
    Microsoft.AspNetCore.SignalR.Client.Core, Microsoft.AspNetCore.SignalR.Common,
    and Microsoft.Data.SqlClient.
    
    Added a new table with ID 'dt-buttons' and sample data to Index.cshtml,
    along with a script section for customPopper.js.
    
    Updated _Layout.cshtml to include a new stylesheet for DataTables buttons
    and adjusted script loading order. Added new scripts for DataTables buttons.
    
    Updated libman.json to include new libraries from cdnjs: cleave.js,
    datatables.net-buttons-bs5, datatables.net-buttons, and font-awesome.
    
    Modified CustomPopper.css to adjust padding and width properties for
    .popper-popup and .popper-popup a classes.
    
    Updated CustomDatatable.js to initialize a new DataTable with ID 'dt-buttons'
    and added a custom button that triggers an alert on click.

--- RMS_FRONTEND/wwwroot/js/CustomDatatable.js
+++ RMS_FRONTEND/wwwroot/js/CustomDatatable.js
@@ -1,3 +1,15 @@
 ﻿$("#dt-basic").DataTable({
     searching: true
 });
+
+    $('#dt-buttons').DataTable({
+        dom: '<"row my-2"<"col-md-6"B><"col-md-6"f>>rtip',
+        buttons: [
+            {
+                text: 'Custom Button',
+                action: function (e, dt, node, config) {
+                    alert('Custom Button Clicked');
+                }
+            }
+        ]
+    });
