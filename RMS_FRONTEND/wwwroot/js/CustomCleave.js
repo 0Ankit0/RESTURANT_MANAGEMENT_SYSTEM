6ecfc85e41c0168a9d20088aef209004cc6537e5
0ankit0 <ankitpaudel.1373@gmail.com>
2024-08-14 16:12:50 +0545

    added cleave and barcode
    -added custom input formatting for money,phone and date
    -added barcode generation code and function to generate barcode

---
+++ RMS_FRONTEND/wwwroot/js/CustomCleave.js
@@ -0,0 +1,19 @@
+﻿new Cleave('.cleavePhone', {
+    prefix: '+977',
+    delimiter: ' ',
+    phone: true,
+    phoneRegionCode: 'NP'
+});
+
+new Cleave('.cleaveDate', {
+    date: true,
+    delimiter: '-',
+    datePattern: ['d', 'm', 'Y']
+});
+
+new Cleave('.cleaveMoney', {
+    prefix: 'RS',
+    delimiter: '.',
+    numeral: true,
+    numeralThousandsGroupStyle: 'lakh'
+});
---
+++ RMS_FRONTEND/wwwroot/js/CustomJsBarcode.js
@@ -0,0 +1,38 @@
+﻿$(".barcodeText").on("keyup", function (e) {
+    var BarCodeText = $(this).val();
+    JsBarcode("#barcodeImage", BarCodeText, {
+        fontSize: 15,
+        font: "monofont", //change the font
+        height: 55,
+        width: 2, //set the width of the bar
+        lineColor: "black",  //change the color of the bars
+        displayValue: true,
+        textAlign: "center",
+        textPosition: "bottom",
+        textMargin: 10 //set the margin of the text
+    });
+})
+function createBarcode(barcodeValue, width = 2, height = 55) {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    const fontSize = 15;
+    const font = "monofont";
+    const lineColor = "black";
+    const textAlign = "center";
+    const textPosition = "bottom";
+    const textMargin = 10;
+
+    JsBarcode(canvas, barcodeValue, {
+        fontSize,
+        font,
+        height,
+        width,
+        lineColor,
+        displayValue: true,
+        textAlign,
+        textPosition,
+        textMargin,
+    });
+    
+    return canvas;
+}
