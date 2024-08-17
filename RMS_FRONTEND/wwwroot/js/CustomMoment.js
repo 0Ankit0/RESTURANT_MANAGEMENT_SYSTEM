cedc48b26f9f820d92b7b3acedd3e7d6fe30d9ac
0ankit0 <ankitpaudel.1373@gmail.com>
2024-08-17 08:42:47 +0545

    Add Tether section, re-indent script, update CustomMoment.js
    Added a new section in Index.cshtml to introduce the Tether
    JavaScript library, including a heading, descriptive paragraph,
    and hyperlink. Adjusted the indentation of the <script> tag for
    customswiper.js in Index.cshtml for better readability. Updated
    CustomMoment.js with comments and examples demonstrating
    various functionalities of the Moment.js library.

--- RMS_FRONTEND/wwwroot/js/CustomMoment.js
+++ RMS_FRONTEND/wwwroot/js/CustomMoment.js
@@ -1,1 +1,23 @@
-﻿
+﻿//To get the current date and time, just call moment() with no parameters.
+
+var now = moment();
+
+//Add time to current moment
+moment().add(Number, String);
+moment().add(7, 'days'); //parameters are days, months, years, hours, minutes, seconds, milliseconds
+
+//Subtract time from current moment
+moment().subtract(Number, String);
+moment().subtract(7, 'days'); //parameters are days, months, years, hours, minutes, seconds, milliseconds
+
+//Get the start of time
+moment().startOf(String);
+moment().startOf('year'); //parameters are day, month, year, hour, minute, second, millisecond
+
+//Get the end of time
+moment().endOf(String);
+moment().endOf('year'); //parameters are day, month, year, hour, minute, second, millisecond
+
+//Get the fime from another moment
+moment().fromNow();
+moment().fromNow('20111031'); //parameters are date, array
