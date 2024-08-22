c0cf6f2db3a3d1d70c923ca599e755c442f2678a
0ankit0 <ankitpaudel.1373@gmail.com>
2024-08-22 07:59:23 +0545

    Add chart display and update project dependencies
    Updated RMS_FRONTEND.csproj to exclude wwwroot\js\ folder.
    Added chart section with <canvas> in Index.cshtml.
    Removed Chart.js@4.4.1 from libman.json and added cdn link instead; re-added remixicon@4.2.0.
    Created CustomChart.js to initialize a line chart.

---
+++ RMS_FRONTEND/wwwroot/js/CustomChart.js
@@ -0,0 +1,14 @@
+﻿const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("chart-line", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: "rgba(0,0,255,1.0)",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    }
+});
