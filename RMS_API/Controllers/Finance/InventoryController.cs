a310e3b413f3389b521083c3b253e0ca67c36ff6
0ankit0 <ankitpaudel.1373@gmail.com>
2024-09-09 15:36:36 +0545

    added get and post endpoints for inventory

--- RMS_API/Controllers/Finance/InventoryController.cs
+++ RMS_API/Controllers/Finance/InventoryController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RMS_API.Data;
+using RMS_API.Data.Finance;
+using RMS_API.Models.Finance;
+using RMS_API.Models.Users;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,22 +21,75 @@ namespace RMS_API.Controllers.Finance
         }
         // GET: api/<InventoryController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<InventoryModel>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var inventory = await _context.Inventories.Select(
+                    i => new InventoryModel
+                    {
+                        InventoryId = i.InventoryId,
+                        ItemName = i.ItemName,
+                        Unit = i.Unit,
+                        Quantity = i.Quantity,
+                        
+                    }).
+                    ToListAsync();
+                return Ok(inventory);
+
+            }catch(Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+
+            }
         }
 
         // GET api/<InventoryController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<IEnumerable<InventoryModel>>> Get(int id)
         {
-            return "value";
+            try
+            {
+                var inventory = await _context.Inventories
+                    .Where(i => i.InventoryId == id)
+                    .Select(i => new InventoryModel
+                    {
+                        InventoryId = i.InventoryId,
+                        ItemName = i.ItemName,
+                        Unit = i.Unit,
+                        Quantity = i.Quantity,
+                    }).ToListAsync();
+                return Ok(inventory);
+
+            }catch(Exception ex) {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
 
         // POST api/<InventoryController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult> Post([FromBody] InventoryModel inventoryModel)
         {
+            try
+            {
+                Inventory inventory = new Inventory
+                {
+                    ItemName = inventoryModel.ItemName,
+                    Unit = inventoryModel.Unit,
+                    Quantity = inventoryModel.Quantity,
+                    GUID = Guid.NewGuid().ToString(),
+                    CreatedAt = DateTime.Now
+                    
+                };
+                _context.Inventories.Add(inventory);
+                await _context.SaveChangesAsync();
+                return Ok(inventory);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
         }
 
         // PUT api/<InventoryController>/5
