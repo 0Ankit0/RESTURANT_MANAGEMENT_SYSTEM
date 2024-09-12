10c4eac3e047fa5d4ff17f5ed14d3488b07f17b7
0ankit0 <ankitpaudel.1373@gmail.com>
2024-09-12 17:15:15 +0545

    created MapProperties function in customclass to map the property of two models with same name and property

--- RMS_FRONTEND/Classes/CustomFunctions.cs
+++ RMS_FRONTEND/Classes/CustomFunctions.cs
@@ -7,7 +7,10 @@ namespace RMS_FRONTEND.Classes
     {
         string GetRandomString(int length);
         SelectList CreateSelectList<T>(IEnumerable<T> items, string idPropertyName, string namePropertyName) where T : class;
-    }
+
+		public void MapProperties<TSource, TDestination>(TSource source, TDestination destination) where TSource:class where TDestination:class;
+
+	}
 
     public class CustomFunctions : ICustomFunctions
     {
@@ -35,6 +38,21 @@ namespace RMS_FRONTEND.Classes
             });
 
             return new SelectList(selectItems, "Value", "Text");
+        }
+        public void MapProperties<TSource, TDestination>(TSource source, TDestination destination) where TSource : class where TDestination : class
+        {
+            var sourceProperties = typeof(TSource).GetProperties();
+            var destinationProperties = typeof(TDestination).GetProperties();
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                var destinationProperty = destinationProperties.FirstOrDefault(p => p.Name == sourceProperty.Name);
+
+                if (destinationProperty != null)
+                {
+                    destinationProperty.SetValue(destination, sourceProperty.GetValue(source));
+                }
+            }
         }
     }
 }
